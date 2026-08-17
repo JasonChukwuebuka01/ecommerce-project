b22ba6feb23419a14973633156681c19824191b9
jason <jasonchinonso20@gmail.com>
2026-08-17 23:00:20 +0100

    Chore: Imported all routes,cors, server listener

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const productsRouter = require("./src/routes/products.routes");
+const globalError = require("./src/middlewares/error.middleware");
+const connectDb = require("./src/databaseConfig/connectDb");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.Port;
+
+connectDb();
+
+app.use(cors());
+
+
+
+//routes
+app.use("/api/products", productsRouter);
+
+app.get("/", (req, res) => {
+  res.send("Api is healthy");
+});
+
+
+app.use(globalError);
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
